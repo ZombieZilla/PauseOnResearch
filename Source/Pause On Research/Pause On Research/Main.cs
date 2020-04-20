d401d9b1f8c5f1646bcb86a47b0aa940f8ecd57c
ZombieZilla <joshuasander04@gmail.com>
2020-04-20 16:51:43 +1000

    The mod
    Everything actually works

---
+++ Source/Pause On Research/Pause On Research/Main.cs
@@ -0,0 +1,27 @@
+﻿using Verse;
+using RimWorld;
+using System.Reflection;
+using HarmonyLib;
+
+namespace Pause_On_Research
+{
+    [StaticConstructorOnStartup]
+    static class HarmonyPatches
+    {
+        static HarmonyPatches()
+        {
+            var harmony = new Harmony("com.zilla.pauseonresearch");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            Log.Message("launching with logspam™️");
+        }
+    }
+
+    [HarmonyPatch(typeof(ResearchManager), "FinishProject")]
+    class Patch
+    {
+        static void Prefix(bool doCompletionDialog)
+            {
+                Find.TickManager.TogglePaused();
+            }
+    }
+}  
